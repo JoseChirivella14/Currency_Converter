603236090cf8ea194502e25729caa3f70510e682
Jose <jrchg1403@gmail.com>
2014-11-13 17:14:48 -0500

    making some changes on testing

--- test/CurrencyConverter_test.rb
+++ test/CurrencyConverter_test.rb
@@ -1,8 +1,15 @@
 require "minitest/autorun"
 require "CurrencyConverter"
+require "Currency"
 
 class CurrencyConverterTest < MiniTest::Unit::TestCase
-  def test_first_test
-    assert false, "Please add a test here"
+  def test_create_currencycode_and_ammount
+    assert true, Currency.new(150, "USD")
+  end
+
+  def test_equal_amount_and_currencycode
+    test1 = Currency.new(150, "USD")
+    test2 = Currency.new(150, "USD")
+    assert true, test1.equal?(test2)
   end
 end
